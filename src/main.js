d17b5d844ad8167287f7c9bf83411dfda0f2260a
tomekr <tomekr@gmail.com>
2020-02-20 15:32:20 +0100

    Add apollo support

--- src/main.js
+++ src/main.js
@@ -6,13 +6,40 @@ import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import router from './router/router'
 import VueRouter from 'vue-router'
+import { ApolloClient } from "apollo-client";
+import VueApollo from "vue-apollo";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { WebSocketLink } from "apollo-link-ws";
 
 Vue.config.productionTip = false
+const link = new WebSocketLink({
+  uri: "ws://trojek.sytes.net:8089/v1/graphql",
+  options: {
+    reconnect: true,
+    timeout: 60000
+    //connectionParams: () => {
+    //  return { headers: getHeaders() };
+  }
+});
+
+const apolloClient = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin)
 Vue.use(VueRouter)
+Vue.use(VueApollo);
 new Vue({
   render: h => h(App),
   router,
+  apolloProvider,
   components: { App }
 }).$mount('#app')
